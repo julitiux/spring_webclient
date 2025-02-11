3ee9f69cc3bd2d73a1547aea0aa94be86373831c
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-10 23:00:47 -0600

    inject bean GitHubService

--- src/main/java/com/spring_webclient/controller/GitHubController.java
+++ src/main/java/com/spring_webclient/controller/GitHubController.java
@@ -1,4 +1,19 @@
 package com.spring_webclient.controller;
 
+import com.spring_webclient.service.GitHubService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/github")
 public class GitHubController {
+
+  private final GitHubService gitHubService;
+
+  public GitHubController(GitHubService gitHubService) {
+    this.gitHubService = gitHubService;
+  }
+
+  
+
 }
