e2b3e57cf97e49eb94049e77698f1edf7cd2c33a
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-10 22:56:25 -0600

    inject bean WebClient

--- src/main/java/com/spring_webclient/service/GitHubService.java
+++ src/main/java/com/spring_webclient/service/GitHubService.java
@@ -1,4 +1,14 @@
 package com.spring_webclient.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class GitHubService {
+
+  private final WebClient webClient;
+
+  public GitHubService(WebClient.Builder webClient) {
+    this.webClient = webClient.baseUrl("https://api.github.com").build();
+  }
 }
