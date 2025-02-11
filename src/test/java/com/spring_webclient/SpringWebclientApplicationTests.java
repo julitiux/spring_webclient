4a7df8289809e34c097f4a182360ae09eacc748a
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-10 22:44:52 -0600

    starting a new project spring_webclient

---
+++ src/test/java/com/spring_webclient/SpringWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
