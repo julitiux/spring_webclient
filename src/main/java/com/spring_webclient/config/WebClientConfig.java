64c2938d23bb653f8f3a51d99016f8a927e21f9e
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-11 22:02:26 -0600

    implement method webClient. Add the baseUrl and headers by default

--- src/main/java/com/spring_webclient/config/WebClientConfig.java
+++ src/main/java/com/spring_webclient/config/WebClientConfig.java
@@ -1,7 +1,21 @@
 package com.spring_webclient.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+  @Bean
+  public WebClient webClient(WebClient.Builder builder){
+    return builder
+      .baseUrl("https://api.github.com")
+      .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+      .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+      .build();
+  }
+
 }
