000527aebcb4ac46293375a047be4bae15b9dfe6
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-28 13:02:25 -0600

    add WebClient instance with baseUrl 'https://jsonplaceholder.typicode.com'

--- src/main/java/com/spring_webclient/service/JsonPlaceHolderService.java
+++ src/main/java/com/spring_webclient/service/JsonPlaceHolderService.java
@@ -1,7 +1,15 @@
 package com.spring_webclient.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class JsonPlaceHolderService {
+
+  private final WebClient webClient;
+
+  public JsonPlaceHolderService(WebClient.Builder webClient) {
+    this.webClient = webClient.baseUrl("https://jsonplaceholder.typicode.com").build();
+  }
+
 }
