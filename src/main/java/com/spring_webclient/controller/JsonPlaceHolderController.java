780c4404b6c92c5901dd9fe4ceaf22ad1bd96467
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-02-28 18:59:10 -0600

    implement controller JsonPlaceHolderController

---
+++ src/main/java/com/spring_webclient/controller/JsonPlaceHolderController.java
@@ -0,0 +1,9 @@
+package com.spring_webclient.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/json_place_holder")
+public class JsonPlaceHolderController {
+}
